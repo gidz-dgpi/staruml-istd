1a8fbf9a5e4f577e678d7c6e72db6f636a93d304
onnohaldar <onno@haldar.nl>
2024-05-08 17:26:35 +0200

    wip

--- xsd-import.js
+++ xsd-import.js
@@ -8,13 +8,45 @@
 const fs = require('fs')
 const convert = require('xml-js');
 
-function addBericht(bericht) {
+/**
+ * 
+ * @param {UMLPackage} berichtenPkg 
+ * @param {Object} bericht 
+ */
+function importBerichtKlassen(berichtenPkg, bericht) {
+    const xsSchema = bericht.elements.find((element) => element.name == 'xs:schema')
+    const xsAnnotation = xsSchema.elements.find((element) => element.name == 'xs:annotation')
+    const xsAppinfo = xsAnnotation.elements.find((element) => element.name == 'xs:appinfo')
+    const berichtInfo = xsAppinfo.elements[1]
+    const berichtInfoElement = berichtInfo.elements[0]
+    const berichtNaam = berichtInfoElement.text.toUpperCase()
+    const berichtPkg = app.factory.createModel({ 
+        id: 'UMLPackage', 
+        parent: berichtenPkg,
+        modelInitializer: elem => {
+            elem.name = berichtNaam
+        } 
+    })
+
+
+}
+
+
+/**
+ * Import iStandaard Bericht from bericht-Object
+ * @param {Object} bericht 
+ */
+function importBericht(bericht) {
     try {
-        fs.writeFileSync('testset/bericht.json', JSON.stringify(bericht, null, '\t'))
-        var berichtenPkg = app.repository.select('Berichten')
-        if (berichtenPkg[0] instanceof type.UMLPackage) {
-                
-        }
+        const project = app.repository.select('@Project')[0]
+        const berichtenPkg = app.factory.createModel({ 
+            id: 'UMLPackage', 
+            parent: project,
+            modelInitializer: elem => {
+                elem.name = 'Berichten'
+            } 
+        })
+        importBerichtKlassen(berichtenPkg, bericht)
         
     } catch (err) {
         fs.writeFileSync('testset/error.json', err.message)
@@ -22,7 +54,10 @@ function addBericht(bericht) {
     }
 }
 
-
+/**
+ * Import XSD File of a iStandaard Bericht
+ * @param {String} xsdFile 
+ */
 function importBerichtXsdFile(xsdFile) {
 
     try {
@@ -30,7 +65,7 @@ function importBerichtXsdFile(xsdFile) {
         try {
             const bericht = convert.xml2js(xsdStr)
             try {
-                addBericht(bericht)
+                importBericht(bericht)
             } catch (err) {
                 console.error(err);
             }
