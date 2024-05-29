b07ecec623ac70a93200933a588ce47f19a31448
onnohaldar <onno@haldar.nl>
2024-05-29 11:49:19 +0200

    wip

--- test-sheetjs.js
+++ test-sheetjs.js
@@ -6,4 +6,46 @@ const regelSheet = fs.readFileSync(regelSheetFile);
 
 const workbook = XLSX.read(regelSheet);
 
-console.log(workbook.SheetNames[0])
+const regelsBerichtElems = workbook.Sheets[workbook.SheetNames[1]];
+
+BerichtHeader = regelsBerichtElems['A1'].v
+
+if (BerichtHeader == 'Bericht') {
+    var row = 2
+    var berichtNameCell = regelsBerichtElems['A' + row]
+    while (berichtNameCell) {
+        const bericht = berichtNameCell.v
+        var nextBericht = bericht
+        console.log(bericht)
+        
+        while (nextBericht == bericht) {
+            row ++
+            berichtNameCell = regelsBerichtElems['A' + row]
+            nextBericht = berichtNameCell ? berichtNameCell.v : undefined
+
+            if (nextBericht) {
+                const sleutel = regelsBerichtElems['F' + row]
+                
+                if (sleutel) {
+                    
+                    if (sleutel.v == 'ja') {
+                        const berichtKlasse = regelsBerichtElems['B' + row].v
+                        const berichtKlasseElementDataType = regelsBerichtElems['C' + row].v
+                        const berichtKlasseElement = berichtKlasseElementDataType.split('\\')[0]
+                        console.log(berichtKlasseElement)
+                    }
+
+                }
+
+            }
+
+
+
+        }
+
+        
+    }
+
+} else {
+    console.error('Bericht-header NOT found!')
+}
